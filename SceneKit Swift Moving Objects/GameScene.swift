84ca8872a9b5ad64232ec01e2fe0b4eab6003589
Manuel W <emkei@mk-pc.fritz.box>
2014-12-21 12:39:30 +0100

    Removed file that shouldn't be tracked

---
+++ SceneKit Swift Moving Objects/GameScene.swift
@@ -0,0 +1,9 @@
+//
+//  GameScene.swift
+//  SceneKit Swift Moving Objects
+//
+//  Created by Manuel W. on 21.12.14.
+//  Copyright (c) 2014 example code by Manuel Wentenschuh. All rights reserved.
+//
+
+import Foundation
