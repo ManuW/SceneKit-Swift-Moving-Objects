6705da14afece4e79a16a4be93961ad2c62ecc19
Manuel W <emkei@mk-pc.fritz.box>
2014-12-21 11:02:34 +0100

    Initial Commit

---
+++ SceneKit Swift Moving Objects/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SceneKit Swift Moving Objects
+//
+//  Created by Manuel W. on 21.12.14.
+//  Copyright (c) 2014 example code by Manuel Wentenschuh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ SceneKit Swift Moving ObjectsTests/SceneKit_Swift_Moving_ObjectsTests.swift
@@ -0,0 +1,36 @@
+//
+//  SceneKit_Swift_Moving_ObjectsTests.swift
+//  SceneKit Swift Moving ObjectsTests
+//
+//  Created by Manuel W. on 21.12.14.
+//  Copyright (c) 2014 example code by Manuel Wentenschuh. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class SceneKit_Swift_Moving_ObjectsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
